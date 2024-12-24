7a729d95a371120a0e35e0694a6741f9b546d66c
Miriamltn <114506193+Miriamltn@users.noreply.github.com>
2024-12-24 18:22:39 +0100

    Añadida funcionalidad de crear lista de la compra

--- src/app/create-list/create-list.component.ts
+++ src/app/create-list/create-list.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; // Importamos CommonModule
+import { ListaService } from '../lista.service';
 
 @Component({
   selector: 'app-create-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule], // Agregamos CommonModule aquí
   templateUrl: './create-list.component.html',
-  styleUrl: './create-list.component.css'
+  styleUrls: ['./create-list.component.css']
 })
 export class CreateListComponent {
+  mostrarFormulario = false;
+  nombreLista = '';
 
+  constructor(private http: HttpClient, private lista: ListaService) {}
+
+  abrirFormulario() {
+    this.mostrarFormulario = true;
+  }
+
+  cerrarFormulario() {
+    this.mostrarFormulario = false;
+    this.nombreLista = '';
+  }
+
+  crearLista() {
+    if (this.nombreLista.trim() === '') {
+      alert('El nombre de la lista no puede estar vacío.');
+      return;
+    }
+    const listaData = { nombre: this.nombreLista };
+    this.lista.crearLista(this.nombreLista);
+
+  }
 }
